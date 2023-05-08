b0947f21741ad1596e30c2336978f1e9d36bc21e
Anton Kotlyar <kotlaranton551@gmail.com>
2023-05-08 19:44:06 +0300

    rezerv 1.1 (need fix bugs)

--- Program.cs
+++ Program.cs
@@ -18,13 +18,30 @@ struct Element
     public int id;
     public string value;
     public int type;
+    public Side rotation;
+    public Side direction;
 
-    public Element(int id, string value, int type)
+    public Element(int id, string value, int type, Side rotation)
     {
         this.id = id;
         this.value = value;
         this.type = type;
+        this.rotation = rotation;
     }
+    
+    public Element(int id, string value, int type, Side rotation, Side direction)
+    {
+        this.id = id;
+        this.value = value;
+        this.type = type;
+        this.rotation = rotation;
+        this.direction = direction;
+    }
+}
+
+enum Side
+{
+    Vertical, Horizontal, Left, Right, Up, Down, Null
 }
 
 class Tetris
@@ -34,6 +51,7 @@ class Tetris
     bool figureActive = false;
     int latency = 400;
     int id = 0;
+    int currentFigure = -1;
 
     string Key(int code)
     {
@@ -56,7 +74,37 @@ class Tetris
         {
             for(int j = 0; j < field.GetLength(1); j++)
             {
-                field[i, j] = new Element(-1, ".", -1);
+                field[i, j] = new Element(-1, ".", -1, Side.Null);
+            }
+        }
+    }
+
+    private void RotateFigure()
+    {
+
+    }
+
+    public void MoveFigure(Side direction)
+    {
+        if(direction == Side.Left)
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+                if (field[i, 0].value != "." && field[i, 1].value != ".") break;
+               // if (field[i, 0].id == id - 1 || field[i, 0].value != "." && field[i, 1].id == id -1) break;
+
+            for (int j = 0; j < field.GetLength(1); j++)
+            {
+                for (int i = 0; i < field.GetLength(0); i++)
+                {
+                    if (j + 1 != field.GetLength(1) && field[i, j].value == ".")
+                    {
+                        if (field[i, j + 1].value != "." && field[i, j + 1].id == id - 1)
+                        {
+                            field[i, j] = field[i, j + 1];
+                            field[i, j + 1] = new Element(-1, ".", -1, Side.Null);
+                        }
+                    }
+                }
             }
         }
     }
@@ -67,7 +115,8 @@ class Tetris
         {
             figureActive = true;
             Random random = new Random();
-            int r = random.Next(1, 6);
+            //int r = random.Next(1, 6);
+            int r = 1;
 
             if(r == 1)
             {
@@ -75,6 +124,11 @@ class Tetris
                 //  ■ |
                 //  ■ | 
                 //  ■ |
+
+                for (int i = 0; i < 4; i++)
+                    field[i, 4] = new Element(id, "■", 1, Side.Vertical);
+                id++;
+                currentFigure = 1;
             }
             else if(r == 2)
             {
@@ -102,9 +156,22 @@ class Tetris
         }
     }
 
-    private void FallFigure()
+    private bool FallFigure()
     {
-
+        bool falled = false;
+        for (int j = field.GetLength(1)-1; j > 0; j--)
+        { 
+            for (int i = field.GetLength(0)-1; i > 0; i--)
+            {
+                if (field[i, j].value == "." && field[i - 1, j].value != ".")
+                {
+                    field[i, j] = field[i - 1, j];
+                    field[i - 1, j] = new Element(-1, ".", -1, Side.Null);
+                    falled = true;
+                }
+            }
+        }
+        return falled;
     }
 
     private void PrintField()
@@ -120,7 +187,26 @@ class Tetris
                 Console.WriteLine();
             }
             Thread.Sleep(latency);
+
+            if (!FallFigure())
+            {
+                DeactivateFigures();
+            }
         }        
+    }
+
+    private void DeactivateFigures()
+    {
+        for (int i = 0; i < field.GetLength(0); i++)
+        {
+            for (int j = 0; j < field.GetLength(1); j++)
+            {
+                field[i, j].type = -1;
+                field[i, j].id = -1;
+            }
+        }
+        figureActive = false;
+        currentFigure = -1;
     }
 
     private void Input()
@@ -129,7 +215,10 @@ class Tetris
         {
             ConsoleKeyInfo ch = Console.ReadKey(true);
             int code = ch.GetHashCode();
-            Console.WriteLine(code);
+
+            if (Key(code) == "up") RotateFigure();
+            else if (Key(code) == "left") MoveFigure(Side.Left);
+            else if (Key(code) == "right") MoveFigure(Side.Right);
         }        
     }    
 
